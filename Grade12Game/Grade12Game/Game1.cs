9ce1089d6d33b0b9cc14753e9156c174519d6df0
Spotandjake <spotandjake@hotmail.com>
2023-05-17 19:46:01 -0400

    feat: Initial Upload

---
+++ Grade12Game/Grade12Game/Game1.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Grade12Game
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        ModelRenderer FishMen; //Your new Fish model
+        Vector3 cameraPosition = new Vector3(0, 0, 20); //Defines the position of the camera in the 3D space
+        Vector3 cameraRotation = new Vector3(0, 0, 0);
+        float farPlaneDistance = 400f; //Defines your plane distance, the higher, the less 3D 'fog' and the more is displayed
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Model currentModel; //Temporary storage for your new model
+
+            Content.RootDirectory = "Content";
+            currentModel = Content.Load<Model>("Models/dude"); //Loads your new model, point it towards your model
+            FishMen = new ModelRenderer(currentModel);
+            FishMen.PlayAnimation("Take 001");//Play the default swimming animation
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            FishMen.Update(gameTime);
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            FishMen.ModelDraw(GraphicsDevice, cameraPosition, cameraRotation, farPlaneDistance);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Grade12Game/Grade12Game/ModelRenderer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using SkinnedModel;
+
+namespace Grade12Game
+{
+    class ModelRenderer : Microsoft.Xna.Framework.Game
+    {
+        public AnimationPlayer animationPlayer; //Controls the animation, references a method in the pre-loaded project
+        public AnimationClip clip; //Contains the animation clip currently playing
+        public Model currentModel; //Model reference
+        public SkinningData skinningData; //Used by the AnimationPlayer method
+        public Vector3 Translation = new Vector3(0, 0, 0); //Fishes current position on the screen
+        public Vector3 Rotation = new Vector3(0, 5*MathHelper.Pi/6, -MathHelper.Pi/2); //Current rotation
+        public float Scale = .2f; //Current scale
+        public ModelRenderer(Model currentModelInput)
+        {
+            currentModel = currentModelInput;
+            // Look up our custom skinning information.
+            skinningData = currentModel.Tag as SkinningData;
+            if (skinningData == null)
+                throw new InvalidOperationException
+                    ("This model does not contain a SkinningData tag.");
+            // Create an animation player, and start decoding an animation clip.
+            animationPlayer = new AnimationPlayer(skinningData);
+        }
+        public new void Update(GameTime gameTime)
+        {
+            if ((clip != null))
+                animationPlayer.Update(gameTime.ElapsedGameTime, true, Matrix.Identity);
+        }
+        public void ModelDraw(GraphicsDevice device, Vector3 cameraPosition, Vector3 cameraRotation, float farPlaneDistance)
+        {
+            Matrix[] bones = animationPlayer.GetSkinTransforms();
+            for (int i = 0; i < bones.Length; i++)
+            {
+                bones[i] *=
+                      Matrix.CreateRotationX(Rotation.X) //Computes the rotation
+                    * Matrix.CreateRotationY(Rotation.Y)
+                    * Matrix.CreateRotationZ(Rotation.Z)
+                    * Matrix.CreateScale(Scale) //Applys the scale
+                    * Matrix.CreateWorld(Translation, Vector3.Forward, Vector3.Up); //Move the models position
+            }
+
+            float aspectRatio = (float)device.Viewport.Width /
+                                (float)device.Viewport.Height;
+
+            // Compute camera matrices
+            Matrix view = Matrix.CreateLookAt(cameraPosition, Translation, Vector3.Right);
+
+            //Create the 3D projection for this model
+            Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), aspectRatio,
+                1.0f, farPlaneDistance);
+
+            // Render the skinned mesh.
+            foreach (ModelMesh mesh in currentModel.Meshes)
+            {
+                foreach (SkinnedEffect effect in mesh.Effects)
+                {
+                    //effect.Parameters["Bones"].SetValue(bones);
+                    //effect.Parameters["View"].SetValue(view);
+                    //effect.Parameters["Projection"].SetValue(projection);
+                    effect.SetBoneTransforms(bones);
+
+                    effect.View = view;
+                    effect.Projection = projection;
+
+                    effect.EnableDefaultLighting();
+
+                    effect.SpecularColor = new Vector3(0.25f);
+                    effect.SpecularPower = 16;
+                }
+                mesh.Draw();
+            }
+        }
+        public void PlayAnimation(String Animation)
+        {
+            clip = skinningData.AnimationClips[Animation];
+            if (clip != animationPlayer.CurrentClip)
+                animationPlayer.StartClip(clip);
+        }
+    }
+}
